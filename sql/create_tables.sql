1262109e2b374a26cbff1ad5352a7fe2356b8556
Rhizlaine DEGNI <Rhizlaine.degni@gmail.com>
2022-05-02 15:17:13 +0200

    seventh commit

---
+++ sql/create_tables.sql
@@ -0,0 +1,19 @@
+CREATE TABLE LIBRAIRIE
+(ISBN varchar(10), 
+Titre varchar(20),
+Auteur varchar(20),
+Annee year,
+CONSTRAINT pkn_librarie PRIMARY KEY(ISBN)
+);
+
+INSERT INTO librairie(ISBN,Titre,Auteur,Annee)
+VALUES ('34435','Livre 1','Auteur 1',1999);
+
+INSERT INTO librairie(ISBN,Titre,Auteur,Annee)
+VALUES ('23435','Livre 2','Auteur 2',1999);
+
+INSERT INTO librairie(ISBN,Titre,Auteur,Annee)
+VALUES ('13435','Livre 3','Auteur 3',1999);
+
+INSERT INTO librairie(ISBN,Titre,Auteur,Annee)
+VALUES ('65435','Livre 4','Auteur 4',1999);
